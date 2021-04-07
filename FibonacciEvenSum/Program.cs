5c9a45a96bc2b70f5c075c8b92470309daa02956
sawyergobin <sawyergobin@outlook.com>
2021-04-07 08:16:06 -0500

    Added documentation for fibonacci sequence challenge

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -12,26 +12,33 @@ namespace FibonacciEvenSum
         {
             //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+            //Set up the variables I need to be reassigned and input starting values for fib nums
             int num1 = 1;
             int num2 = 2;
             int fibResult = 0;
             int runningTotal = 0;
 
+            //set the upper limit. Could also be a variable if different upper limits were needed
             while (num1+num2 < 4000000)
             {
+                //Emulate the sequence
                 fibResult = num1 + num2;
                 num1 = num2;
                 num2 = fibResult;
                 //Uncomment below for testing
                 //Console.WriteLine(fibResult);
+
+                //check for even nums. If even, add to runningTotal
                 if (fibResult % 2 ==0)
                 {
                     runningTotal += fibResult;
                 }
+                
                 //uncomment below for testing
                 //Console.WriteLine("RT =" +runningTotal);
             }
 
+            //output RT to console.
             Console.WriteLine($"The sum of all even values in the Fibonacci Sequence without any " +
                 $"\nvalues in the sequence surpassing 4,000,000 is {runningTotal:n0}.");
 
